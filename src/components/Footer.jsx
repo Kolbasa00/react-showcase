7ddc3cafdc48e28c065c09a6fa1a5f2f21e5f58b
kolbasa00 <vladik30062006@mail.ru>
2026-04-22 23:57:03 +1000

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,12 @@ function Footer() {
       <div className="py-4 bg-green-900/10">
         <div className="container mx-auto flex justify-between items-center">
           <p>© {new Date().getFullYear()} Copyright Text</p>
-          <a href="#!">Repo</a>
+          <a
+            href="https://github.com/Kolbasa00/react-showcase/tree/main"
+            target="_blank"
+          >
+            Repo
+          </a>
         </div>
       </div>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,14 @@ function Header() {
   return (
     <nav className="bg-[#43A047] text-white shadow-[0_4px_6px_-1px_rgba(0,0,0,0.5)]">
       <div className="px-4 flex justify-between items-center">
-        <a href="#" className="text-3xl">
+        <a href="/" className="text-3xl">
           React Shop
         </a>
         <ul>
           <li>
             <a
-              href="!#"
+              href="https://github.com/Kolbasa00/react-showcase/tree/main"
+              target="_blank"
               className="py-5 px-4 block text-sm transition hover:bg-black/10"
             >
               Repo
